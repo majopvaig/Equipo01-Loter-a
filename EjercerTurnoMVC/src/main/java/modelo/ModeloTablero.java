9835804f9b20f0033c9449f1b640e11e3ce0d72c
majojojo <maria.valdez262775@potros.itson.edu.mx>
2026-09-23 18:34:01 -0700

    Primer commit

---
+++ EjercerTurnoMVC/src/main/java/modelo/ModeloTablero.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author maria
+ */
+public class ModeloTablero {
+    
+    // private List<IObserverTablero> observadores;
+    // private Partida partida;
+    // private Tarjeta tarjetaActual;
+    
+    public void obtenerTarjeta(){}
+    
+    //public Tarjeta getTarjetaActual(){}
+    
+    //pub
+    
+}
